a29fad48bcdacd75474d056a24c732cb596b0e80
ejaz629 <ejaz629@gmail.com>
2025-04-01 12:04:25 +1100

    Update config.py
    Google API Key removed as it is not needed.

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 class AppConfig:
     OPENAI_API_KEY = "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"
-    GOOGLE_API_KEY = "AIzaSyBQkUhUd0XqeH6fRk0hidxHlvOsaVD02e0"
     Google_PROJECT_ID = "sw-supply-chain-sec-dev-1184"
     GEMINI_MODEL = "gemini-1.5-flash-002"
     LOCATION = "australia-southeast1"
