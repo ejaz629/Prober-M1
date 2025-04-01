d872df9851bfc5daa7bf726078dc4bcf527d8d0e
ejaz629 <ejaz629@gmail.com>
2025-04-01 13:28:36 +1100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class AppConfig:
     OPENAI_API_KEY = <YOUR_OPENAI_API_KEY>
     Google_PROJECT_ID = <YOUR_Google_PROJECT_ID>
     GITHUB_TOKEN = <YOUR_GITHUB_PAT_TOKEN> # Needed for GitHub API for retrieving data.
-    GEMINI_MODEL = "gemini-1.5-flash-002" # Your can change to other models from Google as well, however, we have not tested it.
+    GEMINI_MODEL = "gemini-1.5-flash-002" # Your can change to other models from Google as well, however, we have not tested it for other models.
     LOCATION = "australia-southeast1"
     DICTIONARY_PATTERN = r'\{(?:[^{}]++|(?R))*\}'
     VERSION_PATTERN = r'\d+\.\d+\.\d+'
